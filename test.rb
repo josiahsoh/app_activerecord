453ca0176e8fce8d63adc8238f1d1c4c4037038b
Josiah <josiahsoh@gmail.com>
2015-11-19 23:59:59 -0800

    solution

--- test.rb
+++ test.rb
@@ -1,1 +1,6 @@
 require_relative 'app'
+
+student = Student.new(cohort_id: 15, first_name: 'Josiah', last_name: 'Soh', email: 'josiahsoh@gmail.com', gender: 'Male', birthdate: '15 June 1987')
+student.save
+
+p Student.all.last
